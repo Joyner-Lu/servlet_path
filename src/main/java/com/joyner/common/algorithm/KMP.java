cdfdf051871fc3b56dd2ad3cef53224fc9f53cb2
luqingyun <luqingyun@foresee.com>
2020-09-04 14:14:37 +0800

    [*]优化KMP算法

--- src/main/java/com/joyner/common/algorithm/KMP.java
+++ src/main/java/com/joyner/common/algorithm/KMP.java
@@ -21,8 +21,6 @@ import java.util.Arrays;
  */
 public class KMP {
 
-    private static long my_count = 0;
-    private static long they_count = 0;
 
     /**
      * 查询是否subStr存在于str中，如果存在，则返回匹配的第一个index的值
@@ -31,9 +29,10 @@ public class KMP {
      * @param subStr
      * @return 返回-1表示未查询掉
      */
-    public static int findStr(String str, String subStr) {
-        //char[] chars = str.toCharArray();
-        //char[] subChars = subStr.toCharArray();
+    public static int indexOfStr(String str, String subStr) {
+        if (str.length() < 1 || subStr.length() < 1 || subStr.length() > str.length()) {
+            return -1;
+        }
         //获取前缀数组
         int i = 0;
         int[] next = getNext(subStr);
@@ -46,23 +45,26 @@ public class KMP {
                 return -1;
             }
 
-            my_count++;
             char c = str.charAt(j);
             if (i == 0) {
                 char firstChar = subStr.charAt(0);
                 if (c != firstChar) {
                     continue;
                 }
+                if (subLen == 1) {
+                    //证明子串只有一个
+                    return j;
+                }
                 //取下一个字符
                 j++;
                 i++;
+
                 c = str.charAt(j);
             }
 
             //比较剩下的位
             for (; i < subLen; i++) {
 
-                my_count++;
                 char subC = subStr.charAt(i);
                 if (subC != c) {
                     if (i == 0) {
@@ -96,24 +98,111 @@ public class KMP {
     }
 
     /**
+     * 查询是否subBytes存在于bytes中，如果存在，则返回匹配的第一个index的值
+     *
+     * @param bytes
+     * @param subBytes
+     * @return 返回-1表示未查询掉
+     */
+    public static int indexOfByte(byte[] bytes, byte[] subBytes) {
+
+        if (bytes.length < 1 || subBytes.length < 1 || subBytes.length > bytes.length) {
+            return -1;
+        }
+
+        //获取前缀数组
+        int i = 0;
+        int[] next = getNext(subBytes);
+        int len = bytes.length;
+        int subLen = subBytes.length;
+
+        for (int j = 0; j < len; j++) {
+            if ((len - j) < subLen) {
+                //剩余的宽度不足以和子串比较
+                return -1;
+            }
+
+            byte b = bytes[j];
+            if (i == 0) {
+                byte firstByte = subBytes[0];
+                if (b != firstByte) {
+                    continue;
+                }
+                if (subLen == 1) {
+                    //证明子串只有一个
+                    return j;
+                }
+
+                //取下一个字符
+                j++;
+                i++;
+                b = bytes[j];
+            }
+
+            //比较剩下的位
+            for (; i < subLen; i++) {
+
+                byte subByte = subBytes[i];
+                if (subByte != b) {
+                    if (i == 0) {
+                        //取主串的下一个进行比较
+                        break;
+                    }
+                    //与主串的当前位进行比较
+                    int pos = next[i + 1];
+                    //修改位置（减2是因为for里面有+1）
+                    i = pos - 2;
+                    continue;
+                } else {
+
+                    if (i == (subLen - 1)) {
+                        //比较完毕，返回匹配的索引
+                        return j - subLen + 1;
+                    }
+                    //相等进行下一位比较
+                    //取主串的下一个字符
+                    j++;
+                    b = bytes[j];
+                    continue;
+                }
+
+
+            }
+        }
+
+
+        return -1;
+    }
+
+
+    /**
      * 求解KMP的next数组
      *
      * @param substr
      * @return
      */
     private static int[] getNext(String substr) {
+        /**
+         * 如果知道next[j] = t,求next[j+1]
+         * (1)如果Pj = Pt, 则next[j+1] = t + 1 = next[j] + 1
+         * (2)如果 Pj != Pt,则循环设置 t = next[t],知道t = 0或者满足（1）为止， 当t = 0时， next[j + 1] = 1
+         */
+
         int len = substr.length();
+        //下标从1开始
         int[] next = new int[len + 1];
-        // 这个i永远只会往前走，而j会随着不匹配而逐渐等于next[j]
-        int j = 1, t = 0;
+        //第1位不使用
         next[0] = -1;
-        next[1] = 0;//第一个比较特殊，直接规定为0
-        while (j < len) {
-            // t=0 表示当前指向的是第一个元素
-            if (t == 0 || substr.charAt(j) == substr.charAt(t)) {
-                // 前面提到过，前面的匹配，所以让后一个等于前一个的下标+1
-                ++j;
-                ++t;
+        //下标为1也就是实际的第1位，规定是0，因为第一位不匹配，表示与主串的下一位进行比较
+        next[1] = 0;
+
+        int j = 1;
+        int t = next[j];
+
+        while (j < next.length - 1) {
+            if (t == 0 || substr.charAt(j - 1) == substr.charAt(t - 1)) {
+                t++;
+                j++;
                 next[j] = t;
             } else {
                 t = next[t];
@@ -122,54 +211,80 @@ public class KMP {
         return next;
     }
 
-    static int indexOf(char[] source, int sourceOffset, int sourceCount,
-                       char[] target, int targetOffset, int targetCount,
-                       int fromIndex) {
-        if (fromIndex >= sourceCount) {
-            return (targetCount == 0 ? sourceCount : -1);
-        }
-        if (fromIndex < 0) {
-            fromIndex = 0;
-        }
-        if (targetCount == 0) {
-            return fromIndex;
-        }
 
-        char first = target[targetOffset];
-        int max = sourceOffset + (sourceCount - targetCount);
+    /**
+     * 求解KMP的next数组
+     *
+     * @param patterns
+     * @return
+     */
+    private static int[] getNext(byte[] patterns) {
+        /**
+         * 如果知道next[j] = t,求next[j+1]
+         * (1)如果Pj = Pt, 则next[j+1] = t + 1 = next[j] + 1
+         * (2)如果 Pj != Pt,则循环设置 t = next[t],知道t = 0或者满足（1）为止， 当t = 0时， next[j + 1] = 1
+         */
 
-        for (int i = sourceOffset + fromIndex; i <= max; i++) {
-            they_count++;
-            /* Look for first character. */
-            if (source[i] != first) {
-                while (++i <= max && source[i] != first && they_count++ > 0);
-            }
+        int len = patterns.length;
+        //下标从1开始
+        int[] next = new int[len + 1];
+        //第1位不使用
+        next[0] = -1;
+        //下标为1也就是实际的第1位，规定是0，因为第一位不匹配，表示与主串的下一位进行比较
+        next[1] = 0;
 
-            /* Found first character, now look at the rest of v2 */
-            if (i <= max) {
-                int j = i + 1;
-                int end = j + targetCount - 1;
-                for (int k = targetOffset + 1; j < end && source[j]
-                        == target[k] && they_count++ > 0; j++, k++);
+        int j = 1;
+        int t = next[j];
 
-                if (j == end) {
-                    /* Found whole string. */
-                    return i - sourceOffset;
-                }
+        while (j < next.length - 1) {
+            if (t == 0 || patterns[j - 1] == patterns[t - 1]) {
+                t++;
+                j++;
+                next[j] = t;
+            } else {
+                t = next[t];
             }
         }
-        return -1;
+        return next;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+
+        testGetNext();
+
     }
 
 
-    public static void main(String[] args) throws Exception {
+    private static void testByte() throws Exception {
+        byte[] subBytes = new byte[]{7,2};
+        byte[] bytes = new byte[]{7};
+        int i = indexOfByte(bytes, subBytes);
+        System.out.println(i);
+
+    }
+
+    private static void testGetNext() throws Exception {
+        int [] next = getNext("ABABAAABABAA");
+        System.out.println(Arrays.toString(next));
+        System.out.println(Arrays.toString(getNext(new byte[]{8,2,8,3})));
+    }
+
+
+
+    private static void testStr1() throws Exception {
+        int pos = indexOfStr("ABCDEFDMNEFXT", "T");
+        System.out.println(pos);
+    }
+
+    private static void testStr() throws Exception {
         FileInputStream fileInputStream = new FileInputStream("D:\\gitSpace\\servlet_path\\src\\main\\resources\\sfz_zm_data");
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(fileInputStream.available());
         byteArrayOutputStream.write(fileInputStream);
 
         String s = new String(byteArrayOutputStream.toByteArray());
-        //s = "AAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAAC";
-        String targetStr = "keMO";
+        s = "AAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAXAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAAdAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACAAAAAAAAACds";
+        String targetStr = "AAAAAAAAACds";
         char[] source = s.toCharArray();
         int sourceOffset = 0;
         int sourceCount = s.length();
@@ -179,22 +294,18 @@ public class KMP {
         int fromIndex = 0;
         //System.out.println(s);
         long start = System.currentTimeMillis();
-        System.out.println("位置indexOf：" + indexOf(source, sourceOffset, sourceCount,
-        target, targetOffset, targetCount,
-        fromIndex));
+        System.out.println("位置indexOf：" + s.indexOf(targetStr));
         long end = System.currentTimeMillis();
         System.out.println(end - start);
 
         start = System.currentTimeMillis();
-        System.out.println("位置findStr：" + findStr(s, targetStr));
+        System.out.println("位置findStr：" + indexOfStr(s, targetStr));
         end = System.currentTimeMillis();
         System.out.println(end - start);
         /*int pos = findStr("ABCDEFDMNEFXT", "DEF");
         System.out.println(pos);
         System.out.println("ABCDEFDMNEFXT".indexOf("DEF"));*/
         //indexOf("ABCDEFDMNEFXT".toCharArray(), 0, "ABCDEFDMNEFXT".length(), "EFX".toCharArray(), 0, "EFX".length(), 0);
-        System.out.println("my_count:" + my_count);
-        System.out.println("they_count:" + they_count);
     }
 
 }
